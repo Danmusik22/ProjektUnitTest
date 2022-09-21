17cdd507b2f600c09fc19a159e4b944eba829798
Danmusik22 <Danmusik22@hotmail.com>
2022-09-21 12:04:36 +0200

    DagligFast lavet færdig

--- src/ordination/ordination/DagligFast.java
+++ src/ordination/ordination/DagligFast.java
@@ -1,5 +1,38 @@
 package ordination.ordination;
 
-public class DagligFast {
+import java.time.LocalDate;
+
+public class DagligFast extends Ordination {
+
+    private Dosis[] dosis = new Dosis[4];
+
+    public DagligFast(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
+
+    public void createDagligDosis(Dosis morgendosis, Dosis middagdosis, Dosis aftendosis, Dosis natdosis) {
+        dosis[0] = morgendosis;
+        dosis[1] = middagdosis;
+        dosis[2] = aftendosis;
+        dosis[3] = natdosis;
+    }
+    @Override
+    public double samletDosis() {
+        return doegnDosis() * antalDage();
+    }
+
+    @Override
+    public double doegnDosis() {
+        double dagligDosis = 0;
+        for (int i = 0; i < dosis.length; i++) {
+            dagligDosis += dosis[i].getAntal();
+        }
+        return dagligDosis;
+    }
+
+    @Override
+    public String getType() {
+        return "DagligFast";
+    }
     // TODO
 }
