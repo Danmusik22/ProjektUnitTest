911285420e739cdd1b381a845cb73836d00824cc
Studsk <kvejborgrasmus@gmail.com>
2022-09-21 12:04:42 +0200

    Rasmus er DaligSkæv v1.0

--- src/ordination/ordination/DagligSkaev.java
+++ src/ordination/ordination/DagligSkaev.java
@@ -1,12 +1,42 @@
 package ordination.ordination;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 
-public class DagligSkaev {
-    // TODO
+public class DagligSkaev extends Ordination {
+
+    private ArrayList<Dosis> dosis = new ArrayList();
+
+    public DagligSkaev(LocalDate startDen, LocalDate slutDen) {
+        super(startDen, slutDen);
+    }
+
 
 
     public void opretDosis(LocalTime tid, double antal) {
-        // TODO
+        Dosis dosis1 = new Dosis(tid, antal);
+        dosis.add(dosis1);
+    }
+
+    @Override
+    public double samletDosis() {
+        double samletdosis = 0;
+
+        for (Dosis d1 : dosis){
+            samletdosis = samletdosis + d1.getAntal();
+        }
+        return samletdosis;
+    }
+
+    @Override
+    public double doegnDosis() {
+
+        return samletDosis()/antalDage();
+    }
+
+    @Override
+    public String getType() {
+        return "Daglig Skæv";
     }
 }
